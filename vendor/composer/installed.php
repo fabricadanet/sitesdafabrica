c8e74f3850c1a27832a8c89bc888a231c944583f
Márcio Rodriguez <rdrgzma@hotmail.com>
2025-11-18 16:46:54 +0000

    fix custom dominio

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fabricadanet/sistesdafabrica',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9a067242612c390260d0741bb61414ba4d385a52',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fabricadanet/sistesdafabrica' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9a067242612c390260d0741bb61414ba4d385a52',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
